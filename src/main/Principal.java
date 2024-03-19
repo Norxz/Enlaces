603f14a1b18758f03ea6284638bf42c659a9b0c4
Norxz <espinosalozano6819@gmail.com>
2024-03-18 20:36:19 -0500

    Inicial

---
+++ src/main/Principal.java
@@ -0,0 +1,89 @@
+package main;
+
+import methods.StackLLI;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author espin
+ */
+public class Principal {
+
+    /**
+     * Genera los nombres que serán agregados a la lista
+     *
+     * @return
+     */
+    private String generarNombre() {
+        String[] nombres = {"Juan", "Andres", "Carolina", "Sebastián", "Felipe", "Camilo", "Miguel", "Ana", "Alberto", "Paola"};
+        String[] apellidos = {"Parra", "Mejia", "Viana", "Ramírez", "Riveros", "Sanabria", "Alvarez", "Pardo"};
+        return nombres[(int) (Math.random() * nombres.length)] + " " + apellidos[(int) (Math.random() * apellidos.length)];
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int option;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("¡BIENVENIDO A LA LISTA!");
+
+        while (true) {
+            System.out.println("""
+                                       Por favor, ingrese una opción:
+                                       1. Insertar dato en una posición indicada.
+                                       2. Insertar dato al final de la lista
+                                       3. Añadir todos los items de una lista a otra.
+                                       4. Elimina todos los elementos de una lista.
+                                       5. Verifica si el elemento existe en la lista.
+                                       6. Retornar el elemento en la posicion especificada.
+                                       7. Retornar el índice de la primer ocurrencia.
+                                       8. Retornar verdadero si no contiene elementos.
+                                       9. Retornar el índice de la última ocurrencia.
+                                       10. Remover el elemento en la posicion especificada.
+                                       11. Remover la primer ocurrencia del elemento especificado.
+                                       12. Reemplazar el elemento en la posición especificada
+                                       13. Retornar el numero de items.
+                                       14. Retornar la porción indicada de items.
+                                       """);
+            option = in.nextInt();
+            switch (option) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                case 13:
+                    break;
+                case 14:
+                    break;
+                default:
+                    System.out.println("Error. Por favor ingresa una opción válida.");
+
+            }
+        }
+    }
+
+}
---
+++ src/methods/StackLLI.java
@@ -0,0 +1,128 @@
+package methods;
+
+import java.util.Iterator;
+
+/**
+ * Esta clase implementa la pila mediante nodos.
+ *
+ * @param <Item> Tipo de los elementos almacenados en la pila.
+ *
+ * @author Norxz
+ */
+public class StackLLI<Item> implements Iterable<Item> {
+
+    Node first;
+    Node last;
+    int count;
+
+    private class Node {
+
+        Item item;
+        Node next;
+        Node prev;
+    }
+
+    /**
+     * Constructor que inicializa una pila vacía.
+     */
+    public StackLLI() {
+        first = null;
+        last = null;
+        count = 0;
+    }
+
+    /**
+     * Agrega un elemento a la pila.
+     *
+     * @param item El elemento a agregar.
+     */
+    public void push(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        if (oldfirst != null) {
+            oldfirst.prev = first;
+        }
+        if (last == null) {
+            last= first;
+        }else{
+            first.prev = last;
+            last.next = first;
+        }
+        count++;
+    }
+
+    /**
+     * Elimina y retorna el elemento superior de la pila.
+     *
+     * @return El elemento superior de la pila.
+     */
+    public Item pop() {
+        Item item = first.item;
+        first.item = null;
+        first = first.next;
+        if (first != null) {
+            first.prev = last;
+            last.next = first;
+        }else{
+            last = null;
+        }
+        count--;
+        return item;
+    }
+
+    /**
+     * Retorna el elemento superior de la pila sin eliminarlo.
+     *
+     * @return El elemento superior de la pila.
+     */
+    public Item peek() {
+        return first.item;
+    }
+
+    /**
+     * Verifica si la pila está vacía.
+     *
+     * @return true si la pila está vacía, false de lo contrario.
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    /**
+     * Retorna el número de elementos en la pila.
+     *
+     * @return El número de elementos en la pila.
+     */
+    public int size() {
+        return count;
+    }
+
+    /**
+     * Retorna un iterador sobre los elementos de la pila.
+     *
+     * @return Un iterador sobre los elementos de la pila.
+     */
+    @Override
+    public Iterator<Item> iterator() {
+        return new LLIterator();
+    }
+
+    private class LLIterator implements Iterator<Item> {
+
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current  != null;
+        }
+
+        @Override
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+}
